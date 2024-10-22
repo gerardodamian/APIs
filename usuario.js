7d1cdd682f9e910e96e05c1de448ca4bf62c4217
Gerardo Montini <germontini@hotmail.com>
2024-10-22 18:46:06 -0300

    first commit

---
+++ usuario.js
@@ -0,0 +1,28 @@
+const aplicacion = document.querySelector('.container')
+const getUrl = new URLSearchParams(window.location.search)
+id = getUrl.get('id')
+
+const url = "https://jsonplaceholder.typicode.com/users";
+console.log(`${url}/${id}`);
+
+fetch(`${url}/${id}`)
+.then(res => res.json())
+.then(data => {
+    const name = document.createElement('p')
+    name.innerHTML = data.name
+    const email = document.createElement("p");
+    email.innerHTML = data.email
+    const username = document.createElement('p')
+    username.innerHTML = data.username
+    const phone = document.createElement("p");
+    phone.innerHTML = data.phone
+    const address = document.createElement("p");
+    address.innerHTML = data.address.street
+    aplicacion.appendChild(name)
+    aplicacion.appendChild(email);
+    aplicacion.appendChild(username);
+    aplicacion.appendChild(phone);
+    aplicacion.appendChild(address);
+})
+
+.catch(err => console.log(err))
